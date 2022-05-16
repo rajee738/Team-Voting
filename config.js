fd909f0654987b9146506cf5cefeda56c18a6f4f
rajee738 <85677273+rajee738@users.noreply.github.com>
2022-05-16 14:03:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAzFttK0kQL8zbBGQKwRW6u2HSAirXmBjM",
+  authDomain: "voting-d7db4.firebaseapp.com",
+  databaseURL: "https://voting-d7db4-default-rtdb.firebaseio.com",
+  projectId: "voting-d7db4",
+  storageBucket: "voting-d7db4.appspot.com",
+  messagingSenderId: "894791700168",
+  appId: "1:894791700168:web:416b4d09fd2d3e17ec9ffd"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
